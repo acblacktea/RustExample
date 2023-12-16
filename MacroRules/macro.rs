d0b605af43f1191d3df0dd25ffa109349eea342e
“yang.liu1@bytedance.com” <yang.liu1@bytedance.com>
2023-12-16 18:40:43 +0800

    update
    Change-Id: I2c5455bda9b6610ba8799bf26b4b18e252b64360

--- MacroRules/macro.rs
+++ MacroRules/macro.rs
@@ -4,6 +4,67 @@ macro_rules! say_hello {
     };
 }
 
+macro_rules! create_function {
+    ($func_name:ident) => {
+        fn $func_name() {
+            println!("You called {:?}()", stringify!($func_name));
+        }
+    };
+}
+
+create_function!(foo);
+create_function!(bar);
+
+macro_rules! print_result {
+    ($expression:expr) => {
+        println!("{:?} = {:?}",
+            stringify!($expression),
+            $expression);
+    };
+}
+
+macro_rules! test {
+    ($left:expr; and $right:expr) => {
+        println!("{:?} and {:?} is {:?}",
+            stringify!($left),
+            stringify!($right),
+             $left && $right)
+    };
+
+    ($left:expr; or $right:expr) => {
+        println!("{:?} or {:?} is {:?}",
+                 stringify!($left),
+                 stringify!($right),
+                 $left || $right)
+    }
+}
+
+macro_rules! find_min {
+    // Base case:
+    ($x:expr) => ($x);
+    // `$x` followed by at least one `$y,`
+    ($x:expr, $($y:expr),+) => (
+        // Call `find_min!` on the tail `$y`
+        std::cmp::min($x, find_min!($($y),+))
+    )
+}
+
 fn main() {
-    say_hello!()
+    say_hello!();
+    foo();
+    bar();
+
+    print_result!(1u32 + 1);
+
+    print_result!({
+        let x = 1u32;
+        x * x + 2 * x - 1
+    });
+
+    test!(1i32 + 1 == 2i32; and 2i32 * 2 == 4i32);
+    test!(true; or false);
+
+    println!("{}", find_min!(1));
+    println!("{}", find_min!(1 + 2, 2));
+    println!("{}", find_min!(5, 2 * 3, 4));
 }
